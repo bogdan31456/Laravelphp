2e629bae4f696bf5b79a14bc6ad91a6a06da76c1
bogdan31456 <73588568+bogdan31456@users.noreply.github.com>
2022-12-06 14:14:14 +0200

    Lab6

--- app/Http/Controllers/Blog/Admin/ BaseController.php
+++ app/Http/Controllers/Blog/Admin/ BaseController.php
@@ -7,5 +7,12 @@ use App\Http\Controllers\Blog\BaseController as GuestBaseController;
 
 abstract class BaseController extends GuestBaseController
 {
+    /**
+     * BaseController constructor
+     */
+    public function __construct()
+    {
+        //Ініціалізація загальних елементів адмінки
+    }
 
 }
